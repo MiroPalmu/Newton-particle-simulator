0bee22f982260d8883e6895c3c308ed38f5a70c3
Miro Palmu <miro.palmu@helsinki.fi>
2022-07-21 23:16:42 +0300

    Fixex typos

--- NewtonPointSimulation.hpp
+++ NewtonPointSimulation.hpp
@@ -7,13 +7,13 @@ namespace NPS {
 Stores data as units from template arguments.
 Primitive data type is defnied in units.hpp
  */
-template<UnitOf<si::dim_length> coordniate_unit, UnitOf<si::dim_mass> mass_units, UnitOf<si::dim_time> simulation_time_units>
+template<UnitOf<si::dim_length> coordinate_unit, UnitOf<si::dim_mass> mass_units, UnitOf<si::dim_time> simulation_time_units>
 class NewtonPointSimulation {
 
   private:
-  std::vector<x_t<coordniate_unit>> x_coordinates;
-  std::vector<y_t<coordniate_unit>> y_coordinates;
-  std::vector<z_t<coordniate_unit>> z_coordinates;
+  std::vector<x_t<coordinate_unit>> x_coordinates;
+  std::vector<y_t<coordinate_unit>> y_coordinates;
+  std::vector<z_t<coordinate_unit>> z_coordinates;
 
   std::vector<particle_mass_t<mass_units>> particle_masses;
   
